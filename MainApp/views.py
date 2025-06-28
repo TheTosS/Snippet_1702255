f84ec1d443f07b8b40fd19b430d949d18fb18481
TheTosS <jermolajevas@web.de>
2025-06-28 15:02:17 +0200

    login

--- MainApp/views.py
+++ MainApp/views.py
@@ -4,6 +4,7 @@ from django.db.models import F
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
 from MainApp.models import LANG_ICON
+from django.contrib import auth
 
 
 def get_icon(lang):
@@ -77,3 +78,23 @@ def snippet_edit(request, id):
             form.save()
 
         return redirect('snippets-list')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            context = {
+                "errors": ["Некорректные данные"]
+            }
+            return render(request, "pages/index.html", context)
+
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect('home')
